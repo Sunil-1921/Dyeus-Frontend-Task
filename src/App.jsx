64a28f298ecba8d94fd344949b05b545ff877f85
Sunil-1921 <s.sirvi1288@gmail.com>
2022-09-14 00:57:37 +0530

    2nd page or about page added but some modifications required

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Home } from './components/home/Home';
 import './components/Body.scss';
 import { Header } from './components/header/Header';
+import { About } from './components/about/About';
 
 function App() {
   return (
     <>
     <Header/>
     <Home/>
+    <About/>
     </>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './About.scss'
+
+export const About = () => {
+  return (
+    <>
+    <div className='about'>
+        <div className="about--title">What makes us different?</div>
+        <div className="about--heading">Reimagining and rewiring self-care</div>
+        <div className="about--content">You care about conscious consumption, and so do we.
+        <br/><br/>
+        When it comes to your health, “good enough” is not good enough. We’re here to flip the script on that narrative because you deserve the best
+        <br/><br/>
+        We collaborate with researchers and doctors to
+        <br/><br/>
+        <li>
+            Help you get granular about your health and self-care with personalized insights, and
+        </li>
+        <br/>
+        <li>
+            Equip you with simple, sustainable products that get the job done
+        </li>
+        <br/>
+        No to-the-moon-and-back claims in this neck of the woods.
+        <br/><br/>
+        We’re just solving real problems for real people here.</div>
+    </div>
+    </>
+  )
+}
