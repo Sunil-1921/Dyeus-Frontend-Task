ece02378d4af09d7d2929d0a1a5aab4f1d55d2a0
Sunil-1921 <s.sirvi1288@gmail.com>
2022-09-14 21:59:09 +0530

    5th page added or 3rd service added

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FirstService } from './FirstService/FirstService'
 import { SecondService } from './SecondService/SecondService'
+import { ThirdService } from './ThirdService/ThirdService'
 
 export const Service = () => {
   return (
     <>
         <FirstService/>
         <SecondService/>
+        <ThirdService/>
     </>
   )
 }
---
+++ src/components/services/ThirdService/ThirdService.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ThirdService.scss'
+import cal from '../../../img/calendar.png'
+
+export const ThirdService = () => {
+  return (
+    <>
+        <div className='service-3'>
+            <h1 className='service-3--heading'>Track your period</h1>
+            <p className='service-3--content'>Stay in the know with reliable AI-based and science-backed period, ovulation, and PMS predictions. The intuitive design makes tracking your cycle effortless and helps you stay on top of your monthly data.</p>
+            <button className='service-3--btn'><p className='service-3--btn-content'>Track your period on the app</p></button>
+            {/* <div className='service-3--imgs'> */}
+                <img className='service-3--image' src={cal} alt="" />
+            {/* </div> */}
+        </div>
+    </>
+  )
+}
